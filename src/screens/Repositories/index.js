feb264a2663ac5f7dcfa1be9a0bf0d97afd9e3c8
Gustavo Rodrigues de Lima <gustavo.lima@ciss.com.br>
2023-11-08 14:36:43 -0300

    adding the folder android

--- src/screens/Repositories/index.js
+++ src/screens/Repositories/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import styles from "./styles";
-import BackSvg from "../../assets/arrow-back.svg";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function Repositories({ route }) {
   return (
     <View style={styles.container}>
-      <BackSvg width="100%" height="100%" />
+      <Ionicons name="arrow-back" style={{ color: "white" }} size={35} />
     </View>
   );
 }
